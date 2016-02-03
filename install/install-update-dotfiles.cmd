4efa0c256946446980a79b7ae5d495f82a379419
Jesse Sanford <jessesanford@gmail.com>
2016-02-02 22:20:01 -0500

    make it mine

--- install/install-update-dotfiles.cmd
+++ install/install-update-dotfiles.cmd
@@ -1,6 +1,6 @@
 cd /d %USERPROFILE%
 IF NOT EXIST %USERPROFILE%\.dot-files (
-	git clone "git://github.com/joshball/dot-files.git" ".dot-files"
+	git clone "git://github.com/jessesanford/dot-files.git" ".dot-files"
 ) ELSE (
 	cd /d %USERPROFILE%\.dot-files
 	git pull
